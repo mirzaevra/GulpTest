479deecd8a7c060508013c551d7e80358579881a
Rustam Mirzaev <mirzaev.r.a@gmail.com>
2019-03-04 16:25:41 +0300

    new Change

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@
 var gulp = require('gulp');
 var notify = require('gulp-notify');
 var scss = require('gulp-sass');
-var pug = require('gulp-pug');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 var del = require('del');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var cache = require('gulp-cache');
+var gcmq = require('gulp-group-css-media-queries');
+var csso = require('gulp-csso');
 // Пути к файлам
 var sourceDir = 'source';
 var publicDir = 'public';
@@ -26,18 +27,9 @@ gulp.task('scss', function () {
     return gulp.src(path.scss).on('error', notify.onError())
         .pipe(scss().on('error', scss.logError)).on('error', notify.onError())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 10'], {cascade: true}))
-        .pipe(gulp.dest(path.css)).on('error', notify.onError())
+        .pipe(gulp.dest(path.css)).pipe(gcmq()).on('error', notify.onError())
         .pipe(browserSync.reload({stream: true})).on('error', notify.onError());
 });
-//PUG
-// gulp.task('pages', function () {
-//     return gulp.src(path.pug)
-//         .pipe(pug({
-//             pretty: true
-//         }))
-//         .pipe(gulp.dest(sourceDir))
-//         .pipe(browserSync.reload({stream: true}));
-// });
 //Browser Sync
 gulp.task('browser-sync', function () {
     browserSync({
@@ -68,7 +60,6 @@ gulp.task('images', function () {
 //Ватчер
 gulp.task('watch', ['browser-sync', 'scss'], function () {
     gulp.watch(path.scss, ['scss']);
-    //gulp.watch(path.pug, ['pages']);
     gulp.watch(path.html, browserSync.reload);
     gulp.watch(path.js, browserSync.reload);
 });
@@ -78,6 +69,8 @@ gulp.task('build', ['clean', 'images', 'scss'], function () {
     var buildHtml = gulp.src(path.html)
         .pipe(gulp.dest(publicDir));
     var buildCss = gulp.src(path.css + '/**/*')
+        .pipe(gcmq())
+        .pipe(csso())
         .pipe(gulp.dest(publicDir + '/css'));
     var buildJs = gulp.src(path.js)
         .pipe(gulp.dest(publicDir + '/js'));
